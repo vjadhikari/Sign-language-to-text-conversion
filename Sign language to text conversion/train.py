eb0e2d699c93b8f0851e69f4bdc8fe9514f50142
vjadhikari <111988838+vjadhikari@users.noreply.github.com>
2023-05-31 10:21:37 +0530

    Update train.py

--- Sign language to text conversion/train.py
+++ Sign language to text conversion/train.py
@@ -19,9 +19,7 @@ classifier.add(MaxPooling2D(pool_size=(2, 2)))
 classifier.add(Convolution2D(32, (3, 3), activation='relu'))
 # input_shape is going to be the pooled feature maps from the previous convolution layer
 classifier.add(MaxPooling2D(pool_size=(2, 2)))
-#classifier.add(Convolution2D(32, (3, 3), activation='relu'))
-# input_shape is going to be the pooled feature maps from the previous convolution layer
-#classifier.add(MaxPooling2D(pool_size=(2, 2)))
+
 
 # Flattening the layers
 classifier.add(Flatten())
